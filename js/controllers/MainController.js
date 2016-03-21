0212d4d09ca69d02ae0e8d3e6f9a4ea1805eaeed
Igor Luciano <igorluciano@igorluciano.com.br>
2016-03-21 20:49:20 -0300

    Commit 7
    Perfil Pronto

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,22 +1,4 @@
 app.controller('MainController',
 ['$scope', function ($scope) {
-    $scope.showPedidos = true;
-    $scope.showEnderecos = false;
-    $scope.showPerfil = false;
-    
-    $scope.showSectionPedidos = function () {
-        $scope.showPedidos = true;
-        $scope.showEnderecos = false;
-        $scope.showPerfil = false;
-    }
-    $scope.showSectionEnderecos = function () {
-        $scope.showPedidos = false;
-        $scope.showEnderecos = true;
-        $scope.showPerfil = false;
-    }
-    $scope.showSectionPerfil = function () {
-        $scope.showPedidos = false;
-        $scope.showEnderecos = false;
-        $scope.showPerfil = true;
-    }      
+     
 }]);
--- js/controllers/PerfilController.js
+++ js/controllers/PerfilController.js
@@ -1,4 +1,10 @@
 app.controller('PerfilController',
 ['$scope', function ($scope) {
-    
+    $scope.nome = 'Igor Luciano';
+    $scope.telefone = '(11)98201-2392';
+    $scope.email = 'igorluciano@igorluciano.com.br';
+    $scope.cpf = '333.555.888-44';
+    $scope.sexo = 'masculino';
+    $scope.senha = '123456';
+    $scope.dataNascimento = new Date('1980','06','10');
 }]);
